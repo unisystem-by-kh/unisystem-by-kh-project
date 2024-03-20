27e154028e9a78acf40e104e77c6d7046b759915
유찬영 <yoo4young@gmail.com>
2024-03-20 12:13:50 +0900

    댓글 관련 클래스 생성

---
+++ uniSystem/src/main/java/kh/finalpro/project/board/controller/ReplyController.java
@@ -0,0 +1,59 @@
+package kh.finalpro.project.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import kh.finalpro.project.board.model.service.ReplyServiceImpl;
+
+@RestController
+public class ReplyController {
+
+	@Autowired
+	private ReplyServiceImpl service;
+	
+	
+	
+	// 자유 게시판용 ---------------------------------------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 1:1 문의 ---------------------------------------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+}
---
+++ uniSystem/src/main/java/kh/finalpro/project/board/model/dao/ReplyDAO.java
@@ -0,0 +1,58 @@
+package kh.finalpro.project.board.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReplyDAO {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	
+	// 자유 게시판용 ---------------------------------------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 1:1 문의 ---------------------------------------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+}
