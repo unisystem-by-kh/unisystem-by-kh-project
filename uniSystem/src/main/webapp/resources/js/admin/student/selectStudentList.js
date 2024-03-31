23e7c72759b1169db5a2408a12bfee4d0ba21bec
Bamboo <qkfqkqk@gmail.com>
2024-03-31 18:31:43 +0900

    교직원 학생 목록 조회(재적상태 정렬?) 완성

--- uniSystem/src/main/webapp/resources/js/admin/student/selectStudentList.js
+++ uniSystem/src/main/webapp/resources/js/admin/student/selectStudentList.js
@@ -1,22 +1,10 @@
-/* (()=>{
-    fetch("/admin/selectList", {
-        method : 'get',
-        headers : {'Content-Type' : "application/json"},
-        body : 1
-    })
-    .then(resp => resp.text())
-    .then(map=>{
-        console.log(map);
-    })
-    .catch(e=>console.log(e))
-})() */
-
-let rows = document.querySelectorAll('.select-row'); // :checked
-
 const allSelect = document.getElementById("allSelect");
+const tbody = document.querySelector('tbody'); // 처음 학생 조회 페이지 들어왔을때의 tbody 내부 저장
+const tbodyFirstTr = document.querySelector('tbody').firstElementChild; // 처음 학생 조회 페이지 들어왔을때의 tbody의 첫 번째 행 저장
 
 // 학생 학번 클릭 시 배경색 변경
 allSelect.addEventListener("click", ()=>{
+    let rows = document.querySelectorAll('.select-row');
     rows.forEach(row => {
         if(!row.checked){
             row.checked = true;
@@ -26,15 +14,29 @@ allSelect.addEventListener("click", ()=>{
     });
 });
 
-rows.forEach(row => {
-    row.addEventListener("change", ()=>{
-        if(row.checked){
-            row.parentElement.parentElement.parentElement.classList.add('highlight');
-        }else{
+function colorChange(){
+    let rows = document.querySelectorAll('.select-row');
+    rows.forEach(row => {
+        if(!row.checked){
             row.parentElement.parentElement.parentElement.classList.remove('highlight');
         }
+        
     });
-});
+}
+
+function noChange(){
+    let rows = document.querySelectorAll('.select-row');
+    rows.forEach(row => {
+        row.addEventListener("change", ()=>{
+            if(row.checked){
+                row.parentElement.parentElement.parentElement.classList.add('highlight');
+            }else{
+                row.parentElement.parentElement.parentElement.classList.remove('highlight');
+            }
+        });
+    });
+}
+
 
 // 일괄 진급 클릭 시 호출되는 함수
 function openModal() {
@@ -43,7 +45,6 @@ function openModal() {
     const memberNoArr = [];
     checkedNo.forEach(no => {
         if(no.checked){ // 체크 된 학생들만
-            console.log(no.parentElement.firstElementChild.innerText); // 체크 되어있는 학생의 번호
             const memberNo = no.parentElement.firstElementChild.innerText;
             const memberObj = {
                 memberNo : memberNo
@@ -51,7 +52,6 @@ function openModal() {
             memberNoArr.push(memberObj);
         }
     });
-    console.log(memberNoArr.length);
 
     // 진급 승인 시 열리는 confirm
     swal({
@@ -63,7 +63,6 @@ function openModal() {
     })
     .then((willDelete) => {
         if(willDelete){ // 예 버튼 클릭시
-            console.log("진급 승인");
 
             /* 진급 할려는 회원 번호를 가지고 비동기 통신 */
             fetch("/admin/demotion",{
@@ -75,7 +74,6 @@ function openModal() {
             })
             .then(resp => resp.text())
             .then(result => {
-                console.log(result);
                 if(result > 0){
                     swal({
                         title : "진급 승인이 완료되었습니다.",
@@ -101,32 +99,11 @@ function openModal() {
         }
     });
 
-    /* const modal = document.getElementById("modal");
-    modal.style.display = "block"; */
 }
-
-/*  function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-}
-
-function confirmModal() {
-    document.querySelector(".modal-body > div").innerText = "N개의 진급이 승인되었습니다.";
-    document.querySelector(".modal-footer > button").style = "display:none;";
-    // document.querySelector(".confirm-btn").style = "display:block;";
-
-    const confirm = document.querySelector(".approve-btn");
-    confirm.innerText = "확인";
-    confirm.removeAttribute("onclick");
-    confirm.setAttribute("onclick", "closeModal()");
-} */
 
 
 // 재적 상태 '신청'일 때 재적 요청 사유 조회
 function statusChange(requestReason, e){
-    /* console.log(requestReason);
-    console.log(e.target.childNodes[1]);
-    console.log(e.target); */
 
     const checkStatus = e.target.childNodes[1]; // input type='checkbox' 요소
     const status = e.target; // 클릭된 td
@@ -151,84 +128,20 @@ function statusChange(requestReason, e){
 // 검색 시 작동
 const search = document.getElementById("search");
 search.addEventListener("change", (e)=>{
-    console.log(e.target.value);
-    const tbody = document.querySelector('tbody');
 
-    const backupTr = tbody.firstElementChild;
-
-    // console.log(backupTr);
-    // console.log(backupTr.firstElementChild);
-
+    const backupTr = tbodyFirstTr.cloneNode(true);
 
     tbody.innerHTML = '';
-
-    console.log(studentArr);
 
     studentArr.forEach(stu =>{
 
         
         if(stu.memberNo.indexOf(e.target.value) != -1 || stu.memberName.indexOf(e.target.value) != -1){
-            console.log(stu);
-            
-            /* const tr = document.createElement("tr");
-            const noTd = document.createElement("td");
-            const label = document.createElement("label"); */
 
+            // insertTr(backupTr, stu);
 
-            const newTr = backupTr.cloneNode(true);
-            console.log(newTr);
-            console.log(stu.memberNo);
-            // 첫 td 안쪽 span 태그에 memberNo 삽입
-            newTr.firstElementChild.firstElementChild.innerHTML = stu.memberNo + "<input type='checkbox' class='select-row'>"; 
-            console.log(newTr.firstElementChild.firstElementChild);
-            console.log(newTr);
-            // 두 번째 td 안에 a태그 안에 href 변경 및 내용 변경
-            newTr.firstElementChild.nextElementSibling.firstElementChild.setAttribute("href", '/student/'+stu.memberNo+'/selectStudentDetail');
-            newTr.firstElementChild.nextElementSibling.firstElementChild.innerText = stu.memberName;
-            // 세 번째 td 안에 학년 삽입            
-            newTr.firstElementChild.nextElementSibling.nextElementSibling.innerText = stu.memberGrade;
-            // 네 번째 td 안에 학과 삽입
-            newTr.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerText = stu.deptName;
+            tbody.append(insertTr(backupTr, stu));
 
-            // 다섯 번째 td 내용
-            if(stu.requestType != ''){ // 재적 변경 요청이 있을 경우
-                console.log("재적 변경 요청 있을 경우");
-                // newTr.lastElementChild.previousElementSibling.innerText = stu.requestReason;
-                newTr.lastElementChild.previousElementSibling.classList.remove("std-status"); // 만약 class 가 있다면 아래에서 추가해주기 위해 제거
-                newTr.lastElementChild.previousElementSibling.classList.add("std-status"); // 클래스 추가
-                newTr.lastElementChild.previousElementSibling.setAttribute('onClick', "statusChange(" +"\""+ stu.requestReason + "\"" + ", event)"); // 재적 변경 요청 사유가 나오는 함수 추가
-                // console.log(newTr.lastElementChild.previousElementSibling.innerText);
-                // console.log(newTr.lastElementChild.previousElementSibling);
-                switch(stu.requestType){
-                    case 'Y' : newTr.lastElementChild.previousElementSibling.innerHTML = "휴학신청<input type='checkbox' hidden>"; break;
-                    case 'N' : newTr.lastElementChild.previousElementSibling.innerHTML = "복학신청<input type='checkbox' hidden>"; break;
-                    case 'D' : newTr.lastElementChild.previousElementSibling.innerHTML = "자퇴신청<input type='checkbox' hidden>"; break;
-                }
-
-                // <td class="std-status" onClick="statusChange('${student.requestReason}', event)">휴학신청<input type="checkbox" hidden></td>
-            }else{ // 재적 변경 요청이 없을 경우
-                console.log("재적 변경 요청 X");
-
-                newTr.lastElementChild.previousElementSibling.classList.remove("std-status"); // 클래스 제거
-                newTr.lastElementChild.previousElementSibling.removeAttribute("onClick"); // 클릭했을시 재적 변경 요청 사유가 나오는 함수 제거
-
-                switch(stu.memberStatus){
-                    case 'Y' : newTr.lastElementChild.previousElementSibling.innerText = "휴학중"; break;
-                    case 'N' : newTr.lastElementChild.previousElementSibling.innerText = "재학중"; break;
-                    case 'P' : newTr.lastElementChild.previousElementSibling.innerText = "졸업"; break;
-                    case 'D' : newTr.lastElementChild.previousElementSibling.innerText = "중퇴"; break;
-                }
-
-                // <td>휴학중<input type="checkbox" hidden></td>
-
-            } // 다섯 번째 td 내용 변경 끝
-            
-            // 여섯 번째 td 안에 이수 학점 대입
-            newTr.lastElementChild.innerText = stu.classPoint+'/30(한 학년 총 학점)'; // '/30(한 학년 총 학점)' : 추후 변경 요소
-            
-            console.log(newTr);
-            tbody.append(newTr);
-            console.log(tbody);
         }
 
     });
@@ -245,6 +158,127 @@ search.addEventListener("change", (e)=>{
         newTr.append(newTd);
         tbody.append(newTr);
     }
-
-    rows = document.querySelectorAll('.select-row');
+    colorChange();
+    noChange();
 });
+
+
+
+
+
+
+
+/* 재적상태 옵션 */
+const firstSelect = document.getElementById("first_select");
+firstSelect.addEventListener("change", ()=>{
+
+    const backupTr = tbodyFirstTr.cloneNode(true);
+
+    tbody.innerHTML = '';
+
+    switch(firstSelect.value){
+        case 'Y' : console.log('휴학');
+            selectTagFn('Y', backupTr);
+            break;
+        case 'N' : console.log('재학');
+            selectTagFn('N', backupTr);
+            break;
+        case 'P' : console.log('졸업');
+            selectTagFn('P', backupTr);
+            break;
+        case 'D' : console.log('중퇴'); 
+            selectTagFn('D', backupTr);
+            break;
+        case 'YY' : console.log('휴학신청'); 
+            selectTagFn('YY', backupTr);
+            break;
+        case 'NN' : console.log('복학신청'); 
+            selectTagFn('NN', backupTr);
+            break;
+        case 'DD' : console.log('자퇴신청');
+            selectTagFn('DD', backupTr);
+            break;
+    }
+});
+
+/* 재적상태 옵션 변경 시 실행되는 함수 */
+function selectTagFn(status, backupTr){
+    studentArr.forEach(stu=>{
+        if(stu.requestType != ''){
+            console.log("요청사항 타입"+stu.requestType);
+            if(stu.requestType+status.substr(0,1) == status){
+                tbody.append(insertTr(backupTr, stu));
+            }    
+        }else{
+            if(stu.memberStatus == status){
+                tbody.append(insertTr(backupTr, stu));
+            }
+        }
+    });
+    if(tbody.innerHTML == '') tbody.append(emptyTbody());
+}
+
+
+/* 한 행 새로만드는 함수 */
+function insertTr(backupTr, stu){
+    
+    const newTr = backupTr.cloneNode(true); // 깊은 복사
+    // 첫 td 안쪽 span 태그에 memberNo 삽입
+    newTr.firstElementChild.firstElementChild.innerHTML = stu.memberNo + "<input type='checkbox' class='select-row'>"; 
+    // 두 번째 td 안에 a태그 안에 href 변경 및 내용 변경
+    newTr.firstElementChild.nextElementSibling.firstElementChild.setAttribute("href", '/student/'+stu.memberNo+'/selectStudentDetail');
+    newTr.firstElementChild.nextElementSibling.firstElementChild.innerText = stu.memberName;
+    // 세 번째 td 안에 학년 삽입            
+    newTr.firstElementChild.nextElementSibling.nextElementSibling.innerText = stu.memberGrade;
+    // 네 번째 td 안에 학과 삽입
+    newTr.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerText = stu.deptName;
+
+    // 다섯 번째 td 내용
+    if(stu.requestType != ''){ // 재적 변경 요청이 있을 경우
+
+        newTr.lastElementChild.previousElementSibling.classList.remove("std-status"); // 만약 class 가 있다면 아래에서 추가해주기 위해 제거
+        newTr.lastElementChild.previousElementSibling.classList.add("std-status"); // 클래스 추가
+        newTr.lastElementChild.previousElementSibling.setAttribute('onClick', "statusChange(" +"\""+ stu.requestReason + "\"" + ", event)"); // 재적 변경 요청 사유가 나오는 함수 추가
+
+        switch(stu.requestType){
+            case 'Y' : newTr.lastElementChild.previousElementSibling.innerHTML = "휴학신청<input type='checkbox' hidden>"; break;
+            case 'N' : newTr.lastElementChild.previousElementSibling.innerHTML = "복학신청<input type='checkbox' hidden>"; break;
+            case 'D' : newTr.lastElementChild.previousElementSibling.innerHTML = "자퇴신청<input type='checkbox' hidden>"; break;
+        }
+
+    }else{ // 재적 변경 요청이 없을 경우
+
+        newTr.lastElementChild.previousElementSibling.classList.remove("std-status"); // 클래스 제거
+        newTr.lastElementChild.previousElementSibling.removeAttribute("onClick"); // 클릭했을시 재적 변경 요청 사유가 나오는 함수 제거
+
+        switch(stu.memberStatus){
+            case 'Y' : newTr.lastElementChild.previousElementSibling.innerText = "휴학중"; break;
+            case 'N' : newTr.lastElementChild.previousElementSibling.innerText = "재학중"; break;
+            case 'P' : newTr.lastElementChild.previousElementSibling.innerText = "졸업"; break;
+            case 'D' : newTr.lastElementChild.previousElementSibling.innerText = "중퇴"; break;
+        }
+
+
+    } // 다섯 번째 td 내용 변경 끝
+    
+    // 여섯 번째 td 안에 이수 학점 대입
+    newTr.lastElementChild.innerText = stu.classPoint+'/30(한 학년 총 학점)'; // '/30(한 학년 총 학점)' : 추후 변경 요소
+
+    return newTr;
+}
+
+/* 검색, select태그로 조회했을 때 조회 결과가 없다면 실행되는 함수 */
+function emptyTbody(){
+
+    const newTr = document.createElement("tr");
+            
+    const newTd = document.createElement("td");
+    newTd.setAttribute("colspan", '6');
+
+    newTd.innerText = '검색 결과가 없습니다.';
+
+    newTr.append(newTd);
+
+    return newTr;
+}
+noChange();
